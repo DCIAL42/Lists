a0a165ce58866a82046b7e5ccf42924c7d249ba7
DCIAL42 <daniel05cooke@gmail.com>
2026-07-30 21:45:31 +0100

    add icons

---
+++ frontend/src/lib/icons/Cross.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { SvelteHTMLElements } from "svelte/elements";
+
+    let props: SvelteHTMLElements["div"] = $props();
+</script>
+
+<div {...props}>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-x-icon lucide-x"
+    >
+        <path d="M18 6 6 18" />
+        <path d="m6 6 12 12" />
+    </svg>
+</div>
+
+<style>
+    div {
+        display: flex;
+    }
+</style>
