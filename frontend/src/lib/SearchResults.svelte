c51be9a774d5c508a94dbcc7f2d4f66d0a43a172
DCIAL42 <daniel05cooke@gmail.com>
2026-07-30 21:44:36 +0100

    update and add components for search

--- frontend/src/lib/SearchResults.svelte
+++ frontend/src/lib/SearchResults.svelte
@@ -1,9 +1,31 @@
-<script>
-    let { children } = $props();
+<script lang="ts">
+    import type { SvelteHTMLElements } from "svelte/elements";
+    import type { Snippet } from "svelte";
+    let {
+        children,
+        ...props
+    }: { children: Snippet<[]> } & SvelteHTMLElements["div"] = $props();
 </script>
 
-<div
-    style="display: flex; flex-direction: column; gap: 10px; align-items: center;"
->
+<div {...props} class="scrollable-element">
     {@render children()}
 </div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        align-items: center;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        top: 100%;
+        overflow-y: auto;
+        max-height: 500px;
+        padding: 10px;
+        border: 2px solid var(--border);
+        border-radius: 4px;
+        background-color: var(--background);
+    }
+</style>
