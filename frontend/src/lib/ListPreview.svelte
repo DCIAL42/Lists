91d4b18b6d281fdcb9c132ca2c8337df8d891b91
DCIAL42 <daniel05cooke@gmail.com>
2026-08-01 21:44:16 +0100

    update typing on listpreview component

--- frontend/src/lib/ListPreview.svelte
+++ frontend/src/lib/ListPreview.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Card from "./components/Card.svelte";
+    import type { List } from "$lib/types";
 
-    let { list } = $props();
+    let { list }: { list: List } = $props();
 </script>
 
 <Card title={list.title} />
