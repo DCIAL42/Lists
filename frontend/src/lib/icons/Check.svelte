018c64e313da3484614bc60cde7d687b3254b319
DCIAL42 <daniel05cooke@gmail.com>
2026-08-11 18:13:29 +0100

    add icons for tracking

---
+++ frontend/src/lib/icons/Check.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { SvelteHTMLElements } from "svelte/elements";
+
+    let props: SvelteHTMLElements["div"] = $props();
+</script>
+
+<div {...props}>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-check-icon lucide-check"
+    >
+        <path d="M20 6 9 17l-5-5" />
+    </svg>
+</div>
+
+<style>
+    div {
+        display: flex;
+    }
+</style>
