2fca5b160fef8e6c3b3c6011b9b13c4323a54bcb
DCIAL42 <daniel05cooke@gmail.com>
2026-08-26 18:41:59 +0100

    minor changes

--- backend/main.go
+++ backend/main.go
@@ -8,16 +8,22 @@ import (
 )
 
 func main() {
-	r, err := app.SetupRouter()
+	log.Printf("PORT=%q", os.Getenv("PORT"))
+	log.Printf("VERCEL=%q", os.Getenv("VERCEL"))
+	log.Printf("VERCEL_ENV=%q", os.Getenv("VERCEL_ENV"))
 
+	r, err := app.SetupRouter()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "38247"
+		log.Fatal("PORT environment variable is missing")
 	}
+
+	log.Printf("Starting server on :%s", port)
+
 	if err := r.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
