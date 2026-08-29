946f3e3d681400dc270967ef538a48d481d032eb
DCIAL42 <daniel05cooke@gmail.com>
2026-08-29 09:57:29 +0100

    new favicon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import favicon from "$lib/assets/favicon.svg";
+    import favicon from "$lib/assets/favicon.ico";
     import { ClerkProvider } from "svelte-clerk";
     import "../app.css";
     import { ModeWatcher } from "mode-watcher";
